36be424173a4b1140ce5ad5ddcbe6dbfa7e98065
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2019-03-11 15:54:14 +0300

    WebApiCrudOperations

---
+++ Programming.API/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using Programming.DAL;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Programming.API.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        CustomersDAL customerDAL = new CustomersDAL();
+        public IEnumerable<Customers> Get()//Tüm Müşterileri Alalım
+        {
+            return customerDAL.GetAllCustomers();
+
+        }
+
+        public Customers Get(int id) //id ile gelen müşteriyi alalım
+        {
+            return customerDAL.GetCustomerById(id);
+
+        }
+
+        public Customers Post(Customers customer) //Dışarıdan bir customer nesnesi gelecek bir form dolduracak kullanan Kişi -customer bilgisi
+        {
+
+            return customerDAL.CreateCustomer(customer);
+        }
+
+        public Customers Put(int id, Customers customer) //Güncellemek için kullanılır.
+        {
+            return customerDAL.UpdateCustomer(id, customer);
+        }
+        
+        public void Delete(int id)
+        {
+            customerDAL.DeleteCustomer(id);
+        }
+
+
+
+
+    }
+}
---
+++ Programming.DAL/CustomersDAL.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Programming.DAL
+{
+    public class CustomersDAL
+    {
+        NorthwindEntities db = new NorthwindEntities();//database'e bağlanmam için oluşturduğum bir class
+        public IEnumerable<Customers> GetAllCustomers()//Tüm Müşterileri Döndürelim
+        {
+            return db.Customers;
+        }
+
+        public Customers GetCustomerById(int id) //id ile müşteri bulduran metodu yazalım
+        {
+            return db.Customers.Find(id);
+
+        }
+
+        public Customers CreateCustomer(Customers customer) //Müşteri eklemek için create metodu yazalım
+        {
+
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            return customer;
+
+        }
+
+        public Customers UpdateCustomer(int id, Customers customer)//Müşteri güncellemek için create metodu yazalım
+        {
+            db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return customer;
+
+        }
+
+        public void DeleteCustomer(int id)//Müşteri silmek için delete metodu yazalım
+        {
+            db.Customers.Remove(db.Customers.Find(id));
+            db.SaveChanges();
+
+
+        }
+
+
+
+    }
+}
